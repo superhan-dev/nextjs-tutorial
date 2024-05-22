331fa64d15dd146f2cd8fbad6a8ca44ae87315be
superhan-dev <superhan.dev@gmail.com>
2024-05-22 23:11:31 +1000

    connected to liveblocks and make skeleton

---
+++ app/board/[boardId]/_component/participants.tsx
@@ -0,0 +1,18 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+
+const Participants = () => {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
+      List of users
+    </div>
+  );
+};
+
+export default Participants;
+
+Participants.Skeleton = function ParticipantsSkeleton() {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 items-center shadow-md w-[100px]" />
+  );
+};
