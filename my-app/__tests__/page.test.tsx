fcdc277b558c093756b98557bc9b5b2a5e1004de
superhan-dev <superhan.dev@gmail.com>
2025-05-22 22:48:59 +0900

    test 초기 셋팅팅

---
+++ my-app/__tests__/page.test.tsx
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Page from "../src/app/dashboard/page";
+
+describe("Dashboard Page", () => {
+  it("renders the dashboard page", () => {
+    render(<Page />);
+    const heading = screen.getByRole("heading", { name: /dashboard/i });
+    const paragraph = screen.getByText(/welcome to the dashboard!/i);
+    expect(heading).toBeInTheDocument();
+    expect(paragraph).toBeInTheDocument();
+  });
+});
