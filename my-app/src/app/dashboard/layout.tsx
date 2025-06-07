0c54b3f442e4e013565e91f2e44712db3702d28d
superhan-dev <superhan.dev@gmail.com>
2025-06-07 12:31:20 +0900

    heatmapjs 적용방법 확인

--- my-app/src/app/dashboard/layout.tsx
+++ my-app/src/app/dashboard/layout.tsx
@@ -4,13 +4,11 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <div>How it works?</div>
-        {/* Layout UI */}
-        {/* Place children where you want to render a page or nested layout */}
-        <main>{children}</main>
-      </body>
-    </html>
+    <div>
+      <div>How it works?</div>
+      {/* Layout UI */}
+      {/* Place children where you want to render a page or nested layout */}
+      <main>{children}</main>
+    </div>
   );
 }
